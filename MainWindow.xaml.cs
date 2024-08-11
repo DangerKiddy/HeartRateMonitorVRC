828a231d5e3da5aa6afb74821aad3898bb4db003
DangerKiddy <42438297+DangerKiddy@users.noreply.github.com>
2024-08-11 17:08:41 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+
+namespace HeartRateMonitorVRC
+{
+    public partial class MainWindow : Window
+    {
+        private enum Fade
+        {
+            In,
+            Out
+        }
+
+        private static DeviceInformation hrDeviceInfo;
+        private static BluetoothLEDevice hrDevice;
+        private static GattCharacteristic hrGatt;
+        private static OSC osc;
+
+        private static int lastSentBpm = 0;
+        private static int currentBpm = 0;
+
+        private static DeviceInformationCollection pairedDevices;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SelectDeviceButton.IsEnabled = false;
+
+            ScanForDevicesAsync();
+            UpdateBPMText();
+            EmulateBeatEffect();
+
+            osc = new OSC();
+        }
+
+        private async void UpdateBPMText()
+        {
+            while (true)
+            {
+                BPMText.Text = $"{currentBpm} BPM";
+
+                await Task.Delay(100);
+            }
+        }
+
+        private async void EmulateBeatEffect()
+        {
+            while (true)
+            {
+                var nextBeat = currentBpm == 0 ? 100 : (int)((1f / currentBpm) * 1000) * 60;
+
+                if (osc != null && hrDevice != null && hrGatt != null)
+                {
+                    var showAnim = new DoubleAnimation();
+                    showAnim.From = 1;
+                    showAnim.To = .25f;
+                    showAnim.Duration = TimeSpan.FromMilliseconds(nextBeat);
+                    BPMText.BeginAnimation(OpacityProperty, showAnim);
+                }
+
+                await Task.Delay(nextBeat);
+            }
+        }
+
+        private void Debug(string text, bool ignoreStatus = false)
+        {
+            Trace.WriteLine(text);
+
+            StatusText.Dispatcher.Invoke(new Action(() =>
+            {
+                StatusText.Text = text;
+            }));
+        }
+
+        private async void ScanForDevicesAsync()
+        {
+            Debug("Scanning");
+            var deviceSelector = BluetoothLEDevice.GetDeviceSelector();
+            pairedDevices = await DeviceInformation.FindAllAsync(deviceSelector);
+
+            Debug("Scan complete, please select device");
+            SelectDeviceButton.IsEnabled = true;
+        }
+
+        private async Task<BluetoothLEDevice> ConnectToDeviceAsync(string deviceId)
+        {
+            var device = await BluetoothLEDevice.FromIdAsync(deviceId);
+            if (device == null)
+            {
+                Debug("Failed connecting to device.");
+                return null;
+            }
+
+            
+            device.ConnectionStatusChanged += Device_ConnectionStatusChanged;
+
+            Debug($"Connected to: {device.Name}");
+            return device;
+        }
+
+        private async void Device_ConnectionStatusChanged(BluetoothLEDevice sender, object args)
+        {
+            if (sender.ConnectionStatus == BluetoothConnectionStatus.Disconnected)
+            {
+                Debug("Device disconnected. Reconnecting...", true);
+
+                await TryPairWithDevice(hrDeviceInfo, true);
+            }
+        }
+
+        private async Task<GattCharacteristic> GetHeartRateCharacteristicAsync(BluetoothLEDevice device, bool reconnecting = false)
+        {
+            var heartRateServiceUuid = GattServiceUuids.HeartRate;
+            var heartRateMeasurementCharacteristicUuid = GattCharacteristicUuids.HeartRateMeasurement;
+
+            var servicesResult = await device.GetGattServicesForUuidAsync(heartRateServiceUuid);
+            if (servicesResult.Status != GattCommunicationStatus.Success || servicesResult.Services.Count == 0)
+            {
+                Debug("Failed getting HR service.");
+                return null;
+            }
+
+            var service = servicesResult.Services[0];
+
+            var characteristicsResult = await service.GetCharacteristicsForUuidAsync(heartRateMeasurementCharacteristicUuid);
+            if (characteristicsResult.Status != GattCommunicationStatus.Success || characteristicsResult.Characteristics.Count == 0)
+            {
+                Debug("Unable to get characteristics.");
+
+                if (reconnecting)
+                {
+                    await Task.Delay(1000);
+
+                    await TryPairWithDevice(hrDeviceInfo);
+                }
+
+                return null;
+            }
+
+            return characteristicsResult.Characteristics[0];
+        }
+
+        private async Task<bool> SubscribeToHeartRateNotificationsAsync(GattCharacteristic characteristic)
+        {
+            characteristic.ValueChanged += Characteristic_ValueChanged;
+
+            var status = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                GattClientCharacteristicConfigurationDescriptorValue.Notify);
+
+            if (status == GattCommunicationStatus.Success)
+            {
+                Debug("Subscribed to HR notifications.");
+            }
+            else
+            {
+                Debug("Failed subscribing to HR notifications.");
+            }
+
+            return status == GattCommunicationStatus.Success;
+        }
+
+        private void Characteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var data = args.CharacteristicValue.ToArray();
+            int heartRate = ParseHeartRate(data);
+
+            lock (this)
+            {
+                currentBpm = heartRate;
+            }
+
+            SendBPMToVRChat(heartRate);
+        }
+
+        private void SendBPMToVRChat(int bpm)
+        {
+            if (osc == null || bpm == lastSentBpm)
+                return;
+
+            lastSentBpm = bpm;
+
+            float hr01 = Remap(bpm, 0, 255, 0, 1);
+            osc.SendFloat("/avatar/parameters/Heartrate2", hr01);
+            osc.SendFloat("/avatar/parameters/HRPercent", hr01);
+            osc.SendFloat("/avatar/parameters/FullHRPercent", Remap(bpm, 0, 255, -1, 1));
+            osc.SendInt("/avatar/parameters/HR", bpm);
+
+            string hrAsStr = bpm.ToString();
+            if (bpm > 99)
+            {
+                osc.SendInt("/avatar/parameters/onesHR", int.Parse(hrAsStr[2].ToString()));
+                osc.SendInt("/avatar/parameters/tensHR", int.Parse(hrAsStr[1].ToString()));
+                osc.SendInt("/avatar/parameters/hundredsHR", int.Parse(hrAsStr[0].ToString()));
+            }
+            else
+            {
+                osc.SendInt("/avatar/parameters/onesHR", int.Parse(hrAsStr[1].ToString()));
+                osc.SendInt("/avatar/parameters/tensHR", int.Parse(hrAsStr[0].ToString()));
+            }
+
+            // Sending it all the time in case if avatar was changed and it should receive new data
+            osc.SendBool("/avatar/parameters/isHRConnected", true);
+            osc.SendBool("/avatar/parameters/isHRActive", true);
+        }
+
+        private int ParseHeartRate(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                return 0;
+
+            // Flags byte
+            var flags = data[0];
+
+            // Heart Rate Measurement value (2 bytes)
+            int heartRate = 0;
+            if ((flags & 0x01) == 0) // Heart Rate Measurement value is 1 byte
+            {
+                heartRate = data[1];
+            }
+            else // Heart Rate Measurement value is 2 bytes
+            {
+                heartRate = BitConverter.ToUInt16(data, 1);
+            }
+
+            return heartRate;
+        }
+
+        public static float Remap(float input, float inputStart, float inputEnd, float outputStart, float outputEnd)
+        {
+            return outputStart + ((outputEnd - outputStart) / (inputEnd - inputStart)) * (input - inputStart);
+        }
+
+        private void Retry_Click(object sender, RoutedEventArgs e)
+        {
+            ElementFadeInOut(BPMText, Fade.Out);
+            ElementFadeInOut(SelectDeviceButton, Fade.In);
+
+            ScanForDevicesAsync();
+        }
+
+        private void SelectDevice_Click(object sender, RoutedEventArgs e)
+        {
+            var contextMenu = new ContextMenu();
+            BuildDeviceSelectionMenu(contextMenu);
+
+            contextMenu.IsOpen = true;
+        }
+
+        private void BuildDeviceSelectionMenu(ContextMenu contextMenu)
+        {
+            foreach (var device in pairedDevices)
+            {
+                var menuItem = BuildDeviceOption(device);
+
+                contextMenu.Items.Add(menuItem);
+            }
+        }
+
+        private MenuItem BuildDeviceOption(DeviceInformation device)
+        {
+            var menuItem = new MenuItem
+            {
+                Header = device.Name
+            };
+
+            menuItem.Click += async (sender, e) =>
+            {
+                SelectDeviceButton.IsEnabled = false;
+                ScanButton.IsEnabled = false;
+
+                bool isSuccess = await TryPairWithDevice(device);
+                if (isSuccess)
+                {
+                    ElementFadeInOut(BPMText, Fade.In);
+                    ElementFadeInOut(SelectDeviceButton, Fade.Out);
+
+                    SelectDeviceButton.IsEnabled = false;
+                }
+                else
+                {
+                    SelectDeviceButton.IsEnabled = true;
+                }
+
+                ScanButton.IsEnabled = true;
+            };
+
+            return menuItem;
+        }
+
+        private async Task<bool> TryPairWithDevice(DeviceInformation device, bool reconnecting = false)
+        {
+            hrDevice = await ConnectToDeviceAsync(device.Id);
+            if (hrDevice == null)
+                return false;
+
+            hrGatt = await GetHeartRateCharacteristicAsync(hrDevice, reconnecting);
+            if (hrGatt == null)
+                return false;
+
+            var success = await SubscribeToHeartRateNotificationsAsync(hrGatt);
+            if (!success)
+            {
+                hrGatt = null;
+                hrDevice.Dispose();
+            }
+
+            hrDeviceInfo = device;
+            return success;
+        }
+
+        private void ElementFadeInOut(UIElement element, Fade fade, int fadeTimeMilliseconds = 500)
+        {
+            var showAnim = new DoubleAnimation();
+            showAnim.From = fade == Fade.In ? 0 : 1;
+            showAnim.To = fade == Fade.In ? 1 : 0;
+            showAnim.Duration = TimeSpan.FromMilliseconds(fadeTimeMilliseconds);
+            element.BeginAnimation(OpacityProperty, showAnim);
+        }
+    }
+}
---
+++ OSC.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using HeartRateMonitorVRC.Properties;
+
+namespace HeartRateMonitorVRC
+{
+    internal class OSC
+    {
+        private UdpClient udp;
+
+        public OSC()
+        {
+            udp = new UdpClient();
+        }
+
+        public void SendFloat(string address, float value)
+        {
+            var sendBack = address + '\0';
+            AlignStringBytes(ref sendBack);
+
+            sendBack += ",f";
+            AlignStringBytes(ref sendBack);
+
+            var buffer = Encoding.ASCII.GetBytes(sendBack);
+            var finalBuffer = new byte[buffer.Length + 4];
+            Array.Copy(buffer, finalBuffer, buffer.Length);
+
+            var bytes = BitConverter.GetBytes(value);
+            finalBuffer[buffer.Length + 0] = bytes[3];
+            finalBuffer[buffer.Length + 1] = bytes[2];
+            finalBuffer[buffer.Length + 2] = bytes[1];
+            finalBuffer[buffer.Length + 3] = bytes[0];
+
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+            udp.Send(finalBuffer, finalBuffer.Length, endPoint);
+        }
+
+        public void SendInt(string address, int value)
+        {
+            var sendBack = address + '\0';
+            AlignStringBytes(ref sendBack);
+
+            sendBack += ",i";
+            AlignStringBytes(ref sendBack);
+
+            var buffer = Encoding.ASCII.GetBytes(sendBack);
+            var finalBuffer = new byte[buffer.Length + 4];
+            Array.Copy(buffer, finalBuffer, buffer.Length);
+
+            var bytes = BitConverter.GetBytes(value);
+            finalBuffer[buffer.Length + 0] = bytes[3];
+            finalBuffer[buffer.Length + 1] = bytes[2];
+            finalBuffer[buffer.Length + 2] = bytes[1];
+            finalBuffer[buffer.Length + 3] = bytes[0];
+
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+            udp.Send(finalBuffer, finalBuffer.Length, endPoint);
+        }
+
+        public void SendBool(string address, bool value)
+        {
+            var sendBack = address + '\0';
+            AlignStringBytes(ref sendBack);
+
+            sendBack += ",";
+            sendBack += value ? "T" : "F";
+            AlignStringBytes(ref sendBack);
+
+            var buffer = Encoding.ASCII.GetBytes(sendBack);
+
+            Trace.WriteLine($"{address}, {buffer.Length}");
+
+            var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
+            udp.Send(buffer, buffer.Length, endPoint);
+        }
+
+        private static void AlignStringBytes(ref string str)
+        {
+            int strLen = str.Length;
+            if (strLen % 4 != 0)
+            {
+                strLen += 4 - (strLen % 4);
+            }
+            else
+            {
+                strLen += 4;
+            }
+
+            for (int i = str.Length; i < strLen; i++)
+            {
+                str += '\0';
+            }
+        }
+    }
+}
